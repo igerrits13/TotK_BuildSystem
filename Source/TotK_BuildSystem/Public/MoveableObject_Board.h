d554f61e6232253ccb1810ca343698d611ad0ba4
Ian Gerrits <77903575+igerrits13@users.noreply.github.com>
2025-07-26 15:35:00 -0600

    Create fuseable object meshes and create new test map

---
+++ Source/TotK_BuildSystem/Public/MoveableObject_Board.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MoveableObject.h"
+#include "MoveableObject_Board.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOTK_BUILDSYSTEM_API AMoveableObject_Board : public AMoveableObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TotK_BuildSystem/Public/MoveableObject_Log.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MoveableObject.h"
+#include "MoveableObject_Log.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOTK_BUILDSYSTEM_API AMoveableObject_Log : public AMoveableObject
+{
+	GENERATED_BODY()
+	
+};
